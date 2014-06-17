d0d449e9c35a94517e02be008bd97517f47535f6
Jan Hovancik <conta.srdr@gmail.com>
2014-06-17 08:55:23 +0000

    twitter auth

--- Gemfile
+++ Gemfile
@@ -38,3 +38,8 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+#Twitter gem
+gem 'twitter'
+
+#FIGARO
+gem 'figaro'
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,7 @@
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["twitter_consumer_key"] 
+  config.consumer_secret     = ENV["twitter_consumer_secret"]
+  config.access_token        = ENV["twitter_access_token"]
+  config.access_token_secret = ENV["twitter_access_token_secret"]
+end
