be7173e3226295372870c85f35d43a6bbe94227c
Jan Hovancik <conta.srdr@gmail.com>
2014-07-12 03:22:55 +0000

    changes to body

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -4,21 +4,24 @@ class EmailProcessor
     to = acceptable_to & email.to
     case to[0]
     when "announce@openbsd.org"
-        a = Article.create(title: email.subject, body: email.raw_html, from: email.from.to_s)
+        a = Article.create(title: email.subject, body: email.body, from: email.from.to_s)
         a.tags.create name: 'openbsd'
         $client.update(email.subject+ " #OpenBSD")
     when "freebsd-announce@freebsd.org"
-        b = Article.create(title: email.subject, body: email.raw_html, from: email.from.to_s)
+        b = Article.create(title: email.subject, body: email.body, from: email.from.to_s)
         b.tags.create name: 'freebsd'
         $client.update(email.subject+ " #FreeBSD")
     when "security-officer@netbsd.org"
-        c = Article.create(title: email.subject, body: email.raw_html, from: email.from.to_s)
+        c = Article.create(title: email.subject, body: email.body, from: email.from.to_s)
         c.tags.create name: 'netbsd'
         $client.update(email.subject+ " #NetBSD")
     when "test@bsdsec.tk"
-      d = Article.create(title: email.subject, body: email.raw_html, from: email.from.to_s)
+      d = Article.create(title: email.subject, body: email.body, from: email.from.to_s)
       d.tags.create name: 'bsdsec'
-      $client.update(email.subject+ " #BSDSec")    
+      p "body " email.body
+      p "raw_html" + email.raw_html
+      p "raw_body" + email.raw_body
+      #$client.update(email.subject+ " #BSDSec")    
     else
         Email.create(from: email.from.to_s, to: email.to.to_s, cc: email.cc.to_s, subject: email.subject, body: email.body)
     end
