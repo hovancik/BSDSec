84cd93ba2af8ce4c55051179e545cc46a2c7feaa
Jan Hovancik <conta.srdr@gmail.com>
2014-07-29 05:33:26 +0000

    activates reddit bot

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -11,11 +11,11 @@ class EmailProcessor
     when "announce@openbsd.org"
       a = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "openbsd")
       $client.update(email.subject+ " #OpenBSD http://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title,"bsdsec",{url: "http://bsdsec.net/articles/#{a.friendly_id}"})
+      reddit_client.submit(a.title,"bsdsec",{url: "http://bsdsec.net/articles/#{a.friendly_id}"})
     when "freebsd-announce@freebsd.org"
       b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "freebsd")
       $client.update(email.subject+ " #FreeBSD http://bsdsec.net/articles/#{b.friendly_id}")
-      #reddit_client.submit(b.title,"bsdsec",{url: "http://bsdsec.net/articles/#{b.friendly_id}"})
+      reddit_client.submit(b.title,"bsdsec",{url: "http://bsdsec.net/articles/#{b.friendly_id}"})
     when "security-officer@netbsd.org"
       c = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "netbsd")
       $client.update(email.subject+ " #NetBSD http://bsdsec.net/articles/#{c.friendly_id}")
