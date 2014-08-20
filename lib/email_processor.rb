e9e1e21c63477fb7f843aebf9f2c651cfb8d7ebf
Jan Hovancik <conta.srdr@gmail.com>
2014-08-20 05:23:32 +0000

    fixes too long twitter message

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -10,16 +10,16 @@ class EmailProcessor
     case n[0]
     when "announce@openbsd.org"
       a = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "openbsd")
-      $client.update(email.subject+ " #OpenBSD http://bsdsec.net/articles/#{a.friendly_id}")
-      reddit_client.submit(a.title,"bsdsec",{url: "http://bsdsec.net/articles/#{a.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #OpenBSD http://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "http://bsdsec.net/articles/#{a.friendly_id}"})
     when "freebsd-announce@freebsd.org"
       b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "freebsd")
-      $client.update(email.subject+ " #FreeBSD http://bsdsec.net/articles/#{b.friendly_id}")
-      reddit_client.submit(b.title,"bsdsec",{url: "http://bsdsec.net/articles/#{b.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #FreeBSD http://bsdsec.net/articles/#{b.friendly_id}")
+      reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "http://bsdsec.net/articles/#{b.friendly_id}"})
     when "security-officer@netbsd.org"
       c = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "netbsd")
-      $client.update(email.subject+ " #NetBSD http://bsdsec.net/articles/#{c.friendly_id}")
-      reddit_client.submit(c.title,"bsdsec",{url: "http://bsdsec.net/articles/#{c.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #NetBSD http://bsdsec.net/articles/#{c.friendly_id}")
+      reddit_client.submit(c.title[0..100] + "...","bsdsec",{url: "http://bsdsec.net/articles/#{c.friendly_id}"})
     else
       Email.create(from: email.from.to_s, to: email.to.to_s, cc: email.cc.to_s, subject: email.subject, body: email.body) unless n[0]==nil
     end
