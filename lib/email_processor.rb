e7cab8c51cc5ffc83db5f5f5d924063094274602
Jan Hovancik <conta.srdr@gmail.com>
2014-10-06 11:48:09 +0000

    adds midnightbsd(not active yet) and pfsense

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,12 +1,11 @@
 class EmailProcessor
   def self.process(email)
     n=""
-    acceptable_to = ["announce@openbsd.org","freebsd-announce@freebsd.org", "netbsd-announce@netbsd.org", "security-advisories@freebsd.org"]
+    acceptable_to = ["announce@openbsd.org","freebsd-announce@freebsd.org", "netbsd-announce@netbsd.org", "security-advisories@freebsd.org", "security-announce@lists.pfsense.org"]
     to = email.to.each {|a| a.downcase!}
     n= acceptable_to & to
     reddit_client = RedditKit::Client.new ENV["reddit_name"], ENV["reddit_pass"] 
     reddit_client.user_agent = "BSDSec.net"
-    puts "\n n = #{n}"
     case n[0]
     when "announce@openbsd.org"
       a = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "openbsd")
@@ -19,10 +18,18 @@ class EmailProcessor
     when "security-advisories@freebsd.org"
       b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "freebsd")
       $client.update(email.subject[0..100] + "... #FreeBSD http://bsdsec.net/articles/#{b.friendly_id}")
+      reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "http://bsdsec.net/articles/#{b.friendly_id}"})
+    when "midnightbsd-security@midnightbsd.org"
+      b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "midnightbsd")
+      $client.update(email.subject[0..100] + "... #MidnightBSD http://bsdsec.net/articles/#{b.friendly_id}")
       reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "http://bsdsec.net/articles/#{b.friendly_id}"})      
     when "netbsd-announce@netbsd.org"
       c = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "netbsd")
       $client.update(email.subject[0..100] + "... #NetBSD http://bsdsec.net/articles/#{c.friendly_id}")
+      reddit_client.submit(c.title[0..100] + "...","bsdsec",{url: "http://bsdsec.net/articles/#{c.friendly_id}"})
+    when "security-announce@lists.pfsense.org"
+      c = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "pfsense")
+      $client.update(email.subject[0..100] + "... #pfSense http://bsdsec.net/articles/#{c.friendly_id}")
       reddit_client.submit(c.title[0..100] + "...","bsdsec",{url: "http://bsdsec.net/articles/#{c.friendly_id}"})
     else
       Email.create(from: email.from.to_s, to: email.to.to_s, cc: email.cc.to_s, subject: email.subject, body: email.body) unless n[0]==nil
