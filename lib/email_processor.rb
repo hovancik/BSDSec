5a8b01340507debd7afadc17c040a95ab3c7fefe
Jan Hovancik <conta.srdr@gmail.com>
2015-02-26 10:59:20 +0000

    comments twitter and reddit for heroku deployment

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -9,32 +9,32 @@ class EmailProcessor
     case n[0]
     when "announce@openbsd.org"
       a = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "openbsd")
-      $client.update(email.subject[0..100] + "... #OpenBSD https://bsdsec.net/articles/#{a.friendly_id}")
-      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
+      #$client.update(email.subject[0..100] + "... #OpenBSD https://bsdsec.net/articles/#{a.friendly_id}")
+      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
     when "freebsd-announce@freebsd.org" || "security-advisories@freebsd.org"
       b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "freebsd")
-      $client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{b.friendly_id}")
-      reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})
+      #$client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{b.friendly_id}")
+      #reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})
     when "security-advisories@freebsd.org"
       b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "freebsd")
-      $client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{b.friendly_id}")
-      reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})
+      #$client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{b.friendly_id}")
+      #reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})
     when "errata-notices@freebsd.org"
       b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "freebsd")
-      $client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{b.friendly_id}")
-      reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})
+      #$client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{b.friendly_id}")
+      #reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})
     when "midnightbsd-security@midnightbsd.org"
       b = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "midnightbsd")
-      $client.update(email.subject[0..100] + "... #MidnightBSD https://bsdsec.net/articles/#{b.friendly_id}")
-      reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})      
+      #$client.update(email.subject[0..100] + "... #MidnightBSD https://bsdsec.net/articles/#{b.friendly_id}")
+      #reddit_client.submit(b.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{b.friendly_id}"})      
     when "netbsd-announce@netbsd.org"
       c = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "netbsd")
-      $client.update(email.subject[0..100] + "... #NetBSD https://bsdsec.net/articles/#{c.friendly_id}")
-      reddit_client.submit(c.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{c.friendly_id}"})
+      #$client.update(email.subject[0..100] + "... #NetBSD https://bsdsec.net/articles/#{c.friendly_id}")
+      #reddit_client.submit(c.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{c.friendly_id}"})
     when "security-announce@lists.pfsense.org"
       c = Article.create(title: email.subject, body: email.body, from: email.from.to_s, tag_list: "pfsense")
-      $client.update(email.subject[0..100] + "... #pfSense https://bsdsec.net/articles/#{c.friendly_id}")
-      reddit_client.submit(c.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{c.friendly_id}"})
+      #$client.update(email.subject[0..100] + "... #pfSense https://bsdsec.net/articles/#{c.friendly_id}")
+      #reddit_client.submit(c.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{c.friendly_id}"})
     else
       Email.create(from: email.from.to_s, to: email.to.to_s, cc: email.cc.to_s, subject: email.subject, body: email.body) #unless n[0]==nil
     end
