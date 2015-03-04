4b6ffe720b7b46c68ce4271c2e03a330c6297db9
Jan Hovancik <conta.srdr@gmail.com>
2015-03-04 15:12:04 +0000

    Rails 4.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,9 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+# Asset digests allow you to set far-future HTTP expiration dates on all assets,
+# yet still be able to expire them through the digest params.
+ config.assets.digest = true
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,9 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
