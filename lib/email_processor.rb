b4eb3ad4c8a67187c4b1b661ae56b5d7ffb09f09
Jan Hovancik <conta.srdr@gmail.com>
2015-03-12 15:39:05 +0000

    uncomment twit an reddt

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -17,32 +17,32 @@ class EmailProcessor
     case n[0]
     when "announce@openbsd.org"
       a = create_article("openbsd")
-      #$client.update(email.subject[0..100] + "... #OpenBSD https://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #OpenBSD https://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
     when "freebsd-announce@freebsd.org"
       a = create_article("freebsd")
-      #$client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
     when "security-advisories@freebsd.org"
       a = create_article("freebsd")
-      #$client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
     when "errata-notices@freebsd.org"
       a = create_article("freebsd")
-      #$client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #FreeBSD https://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
     when "midnightbsd-security@midnightbsd.org"
       a =create_article("midnightbsd")
-      #$client.update(email.subject[0..100] + "... #MidnightBSD https://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})      
+      $client.update(email.subject[0..100] + "... #MidnightBSD https://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})      
     when "netbsd-announce@netbsd.org"
       a = create_article("netbsd")
-      #$client.update(email.subject[0..100] + "... #NetBSD https://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #NetBSD https://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
     when "security-announce@lists.pfsense.org"
       a = create_article("pfsense")
-      #$client.update(email.subject[0..100] + "... #pfSense https://bsdsec.net/articles/#{a.friendly_id}")
-      #reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
+      $client.update(email.subject[0..100] + "... #pfSense https://bsdsec.net/articles/#{a.friendly_id}")
+      reddit_client.submit(a.title[0..100] + "...","bsdsec",{url: "https://bsdsec.net/articles/#{a.friendly_id}"})
     else
       Email.create(from: @email.from[:email].to_s, to: @email.to.map {|a| a[:email].downcase}.to_s, cc: @email.cc.map {|a| a[:email].downcase}.to_s, subject: @email.subject, body: @email.body) #unless n[0]==nil
     end
