cbf0ffad604c562766827a8d2877a8fe6e169925
Jan Hovancik <jan@hovancik.net>
2021-05-10 15:19:38 +0200

    Fix weirdly encoded emails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
     text.strftime("%e %B, %Y")
   end
 
+  def art_body(text)
+    text.lines.map { |c| c.unpack("M*") }.join
+  end
+
   def motif_image_url(url: root_url)
     "https://motif.imgix.com/i?url=#{u url}&image_url=#{u image_url '/img/pankaj-patel-fvMeP4ml4bU-unsplash.jpg' }&color=b1d6f4&logo_url=#{u image_url '/img/bsdsec.png' }&logo_alignment=top%2Ccenter&text_alignment=bottom%2Ccenter&logo_padding=70&font_family=Avenir%20Next%20Demi%2CBold&text_color=1d1d1d"
   end
