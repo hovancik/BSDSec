b558ba6ff55b12f8543f9494dcc68e6853be3f54
Jan Hovancik <jan@hovancik.net>
2021-05-31 14:52:40 +0200

    10 articles per page

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,8 @@ class Article < ApplicationRecord
     has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
 
+    paginates_per 10
+
     def tag_list
       self.tags.collect do |tag|
         tag.name
@@ -17,4 +19,5 @@ class Article < ApplicationRecord
       new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
       self.tags = new_or_found_tags
     end
+
 end
