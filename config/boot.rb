78679bcb9576ab9a7f94e093a4005d763a624658
Jan Hovancik <conta.srdr@gmail.com>
2025-12-12 14:04:54 +0000

    Upgrade Rails to 8.0

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,4 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-
-# Ruby 3.3+ compatibility: require logger before Rails loads
-require "logger"
