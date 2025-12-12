b01f6592a8e7d545db0ffc988fa5743d66bd58bf
Jan Hovancik <conta.srdr@gmail.com>
2025-12-12 13:15:14 +0000

    Upgrade Rails to 7.2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM ruby:3.3.10
-RUN apt-get update -qq && apt-get install -y libpq-dev
+
+# Install dependencies including jemalloc for memory optimization
+RUN apt-get update -qq && apt-get install -y libpq-dev libjemalloc2
+
+# Configure jemalloc for better memory allocation
+ENV LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.2
+ENV MALLOC_CONF=dirty_decay_ms:1000,narenas:2,background_thread:true
+
 RUN mkdir /BSDSec
 WORKDIR /BSDSec
 ADD Gemfile Gemfile.lock /BSDSec/
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2021_05_31_141732) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_12_125849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,7 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2021_05_31_141732) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", precision: nil, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
