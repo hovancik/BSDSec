89254c0785723e82acccc45ac033e8d191851664
Jan Hovancik <conta.srdr@gmail.com>
2025-12-12 12:44:39 +0000

    Upgrade Rails to 7.1

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,5 @@ threads threads_count, threads_count
 
 preload_app!
 
-rackup      DefaultRackup if defined?(DefaultRackup)
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
-
-before_worker_boot do
-  # Worker-specific setup for Rails 4.1 to 5.2, after 5.2 it's not needed
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
